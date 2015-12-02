609cfc3770b95de6afaf614214fc49a4ede50b6b
Jakub Jirutka <jakub@jirutka.cz>
2015-12-03 00:53:21 +0100

    Fix roster-migrate script

--- src/main/scripts/roster-migrate.sh
+++ src/main/scripts/roster-migrate.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-LIB="$DIR/../lib"
+SCRIPT_DIR="$(cd "$(dirname "$(readlink "$0" || echo "$0")")" && pwd -P)"
+LIBS_DIR="${SCRIPT_DIR}/../lib"
 JAVA="${JAVA:-java}"
 
-$JAVA -Djava.ext.dirs="$LIB" -classpath "$JAVA_HOME/lib/ext/*" cz.rdc.devel.jabber.migrate.Main "$@"
-
-if [ $? != 0 ]; then
-    echo ""
-    echo "JAVA_HOME=$JAVA_HOME"
-fi
+$JAVA -Djava.ext.dirs="$LIBS_DIR" -classpath "$JAVA_HOME/lib/ext/*" cz.rdc.devel.jabber.migrate.Main "$@"
+[ $? -eq 0 ] || echo -e "\nJAVA_HOME=$JAVA_HOME"
